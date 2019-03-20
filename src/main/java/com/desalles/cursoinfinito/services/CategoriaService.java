d076b4387e6388e4acf53d601af59bb6a000ee4d
Diego de Salles <diegodesallesb@gmail.com>
2019-03-20 06:49:21 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/desalles/cursoinfinito/services/CategoriaService.java
+++ src/main/java/com/desalles/cursoinfinito/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.desalles.cursoinfinito.domain.Categoria;
 import com.desalles.cursoinfinito.repositories.CategoriaRepository;
+import com.desalles.cursoinfinito.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -26,6 +27,16 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		/*
+		 * if(obj == null) { throw new
+		 * ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " +
+		 * Categoria.class.getName()); }
+		 * 
+		 * Não funciona na versão 2.x.x do Spring devido o Java 7
+		 */
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+								"Objeto não encontrado! Id: " + id +
+								", Tipo: " + Categoria.class.getName()));
 	}
 }
