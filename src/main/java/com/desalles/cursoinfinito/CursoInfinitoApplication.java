c81a042c13a55665eae4efca813f8e68493975eb
Diego de Salles <diegodesallesb@gmail.com>
2019-03-20 05:54:04 -0300

    Criando operação de instanciação

--- src/main/java/com/desalles/cursoinfinito/CursoInfinitoApplication.java
+++ src/main/java/com/desalles/cursoinfinito/CursoInfinitoApplication.java
@@ -1,13 +1,39 @@
 package com.desalles.cursoinfinito;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoInfinitoApplication {
+import com.desalles.cursoinfinito.domain.Categoria;
+import com.desalles.cursoinfinito.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoInfinitoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoInfinitoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		/*
+		 * categoriaRepository.save(Arrays.asList(cat1, cat2));
+		 * 
+		 * Esse comando não funciona no Spring 2.x.x devido ao Java 7
+		 */
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
