3ab301cfe1319804f02f4e9838b823c0a6a86796
Diego de Salles <diegodesallesb@gmail.com>
2019-03-20 06:17:47 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/desalles/cursoinfinito/CursoInfinitoApplication.java
+++ src/main/java/com/desalles/cursoinfinito/CursoInfinitoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.desalles.cursoinfinito.domain.Categoria;
+import com.desalles.cursoinfinito.domain.Produto;
 import com.desalles.cursoinfinito.repositories.CategoriaRepository;
+import com.desalles.cursoinfinito.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoInfinitoApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoInfinitoApplication.class, args);
@@ -26,6 +30,17 @@ public class CursoInfinitoApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		/*
 		 * categoriaRepository.save(Arrays.asList(cat1, cat2));
 		 * 
@@ -33,6 +48,7 @@ public class CursoInfinitoApplication implements CommandLineRunner {
 		 */
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
