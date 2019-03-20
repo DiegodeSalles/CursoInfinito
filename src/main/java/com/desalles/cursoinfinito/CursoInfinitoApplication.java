13326627afe0454e5428e78895c82cf1a1c0316c
Diego de Salles <diegodesallesb@gmail.com>
2019-03-20 07:57:11 -0300

    Estado e cidade, estudo de muitos pra um

--- src/main/java/com/desalles/cursoinfinito/CursoInfinitoApplication.java
+++ src/main/java/com/desalles/cursoinfinito/CursoInfinitoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.desalles.cursoinfinito.domain.Categoria;
+import com.desalles.cursoinfinito.domain.Cidade;
+import com.desalles.cursoinfinito.domain.Estado;
 import com.desalles.cursoinfinito.domain.Produto;
 import com.desalles.cursoinfinito.repositories.CategoriaRepository;
+import com.desalles.cursoinfinito.repositories.CidadeRepository;
+import com.desalles.cursoinfinito.repositories.EstadoRepository;
 import com.desalles.cursoinfinito.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursoInfinitoApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoInfinitoApplication.class, args);
@@ -41,6 +49,16 @@ public class CursoInfinitoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+			
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		/*
 		 * categoriaRepository.save(Arrays.asList(cat1, cat2));
 		 * 
@@ -49,6 +67,8 @@ public class CursoInfinitoApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
---
+++ src/main/java/com/desalles/cursoinfinito/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.desalles.cursoinfinito.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desalles.cursoinfinito.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
